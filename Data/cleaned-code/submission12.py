e6600fe5d0697fceb96c21cdb398ef519a774bf8
Salvador Rodriguez <salvarp03@hotmail.com>
2026-05-29 18:21:36 -0600

    feat: initial cleaning notebook deleted, tree generator notebook added

--- Data/cleaned-code/submission12.py
+++
@@ -1,7 +0,0 @@
-def recursive_factorial(n):
-    if n <= 1:
-        return 1
-    else:
-        return n * recursive_factorial(n-1)
-
-print(recursive_factorial(5))
--- Data/cleaned-code/submission13.py
+++
@@ -1,7 +0,0 @@
-def iterative_factorial(n):
-    result = 1
-    for i in range(2, n+1):
-        result *= i
-    return result
-
-print(iterative_factorial(5))
